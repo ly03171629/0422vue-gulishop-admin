91acb272ae2374144512fd09a20f1e24e0fa6ecb
liuyuan <1316425655@qq.com>
2020-08-26 14:55:16 +0800

    'adminstart'

--- src/views/product/trademark/list.vue
+++ src/views/product/trademark/list.vue
@@ -1,56 +1,144 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
+    <el-button type="primary" icon="el-icon-plus" @click="showAddDialog">添加</el-button>
 
-    <el-table
-      border
-      style="width: 100%;margin:20px 0"
-    >
+    <el-table :data="trademarkList" border style="width: 100%;margin:20px 0">
       <el-table-column label="序号" type="index" width="80" align="center"></el-table-column>
-      <el-table-column prop="" label="品牌名称"></el-table-column>
-      <el-table-column prop="" label="品牌LOGO"></el-table-column>
-      <el-table-column prop="" label="操作"></el-table-column>
+      <el-table-column prop="tmName" label="品牌名称"></el-table-column>
+      <el-table-column prop="logoUrl" label="品牌LOGO">
+        <template slot-scope="{row,$index}">
+          <!-- 当你要展示的数据结构由你说了算 -->
+          <img :src="row.logoUrl" alt style="width:100px;height:60px" />
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="{row,$index}">
+          <el-button type="warning" icon="el-icon-edit" size="mini">修改</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
 
     <el-pagination
       style="text-align:center"
-      :current-page="1"
-      :page-sizes="[10, 20, 50]"
-      :page-size="3"
+      @size-change="handleSizeChange"
+      @current-change="getTrademarkList"
+      :current-page="page"
+      :page-sizes="[3, 5, 10]"
+      :page-size="limit"
       layout=" prev, pager, next, jumper,->, sizes,total"
-      :total="0">
-    </el-pagination>
-    <!--  @size-change="handleSizeChange"
-      @current-change="handleCurrentChange" -->
+      :total="total"
+    ></el-pagination>
+
+    <el-dialog title="添加品牌" :visible.sync="isShowDialog">
+      <el-form :model="form" style="width:80%">
+        <el-form-item label="品牌名称" :label-width="'100px'">
+          <el-input v-model="form.tmName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" :label-width="'100px'">
+          <el-upload
+            class="avatar-uploader"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="form.logoUrl" :src="form.logoUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="isShowDialog = false">取 消</el-button>
+        <el-button type="primary" @click="isShowDialog = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   name: "Trademark",
-  data(){
+  data() {
     return {
-      page:1,
-      limit:3,
-      total:0,
-      trademarkList:[]
-    }
+      page: 1,
+      limit: 3,
+      total: 0,
+      trademarkList: [],
+      isShowDialog: false,
+      form: {
+        tmName: "",
+        logoUrl:''
+      },
+    };
   },
   mounted() {
     // this.$API.trademark.getPageList(1, 3);
-    this.getTrademarkList()
+    this.getTrademarkList();
   },
-  methods:{
-    async getTrademarkList(){
-      const result = await this.$API.trademark.getPageList(this.page,this.limit);
-      if(result.code === 200){
-        this.trademarkList = result.data.records
-        this.total = result.data.total
+  methods: {
+    async getTrademarkList(page = 1) {
+      this.page = page;
+      const result = await this.$API.trademark.getPageList(
+        this.page,
+        this.limit
+      );
+      if (result.code === 200) {
+        this.trademarkList = result.data.records;
+        this.total = result.data.total;
       }
-    }
-  }
+    },
+    handleSizeChange(size) {
+      this.limit = size;
+      this.getTrademarkList();
+    },
+    //点击添加按钮，弹出对话框
+    showAddDialog() {
+      this.isShowDialog = true;
+    },
+
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
 </style>
