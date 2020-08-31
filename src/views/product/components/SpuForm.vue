09e38a1b1b07b93c2b286db8b74a9a1ddd3da85d
liuyuan <1316425655@qq.com>
2020-08-31 11:50:36 +0800

    'spulook'

--- src/views/product/components/SpuForm.vue
+++ src/views/product/components/SpuForm.vue
@@ -7,7 +7,12 @@
 
       <el-form-item label="品牌">
         <el-select placeholder="请选择品牌" v-model="spuInfo.tmId">
-          <el-option :label="trademark.tmName" :value="trademark.id" v-for="(trademark, index) in trademarkList" :key="trademark.id"></el-option>
+          <el-option
+            :label="trademark.tmName"
+            :value="trademark.id"
+            v-for="(trademark, index) in trademarkList"
+            :key="trademark.id"
+          ></el-option>
         </el-select>
       </el-form-item>
 
@@ -35,11 +40,40 @@
           <el-option value></el-option>
         </el-select>
         <el-button type="primary" icon="el-icon-plus">添加销售属性</el-button>
-        <el-table border style="width: 100%;margin-top:20px">
+        <el-table border style="width: 100%;margin-top:20px" :data="spuInfo.spuSaleAttrList">
           <el-table-column align="center" type="index" label="序号" width="80"></el-table-column>
-          <el-table-column prop="prop" label="属性名" width="150"></el-table-column>
-          <el-table-column prop="prop" label="属性值名称列表" width="width"></el-table-column>
-          <el-table-column label="操作" width="150"></el-table-column>
+          <el-table-column prop="saleAttrName" label="属性名" width="150"></el-table-column>
+          <el-table-column label="属性值名称列表" width="width">
+            <template slot-scope="{row,$index}">
+              <el-tag
+                :key="spuSaleAttrValue.id"
+                v-for="(spuSaleAttrValue,index) in row.spuSaleAttrValueList"
+                closable
+                :disable-transitions="false"
+              >{{spuSaleAttrValue.saleAttrValueName}}</el-tag>
+              <!-- @close="handleClose(tag)" -->
+
+              <!-- row.isEidt是后面我们编辑模式和查看模式的依据 我们把这个数据放在属性身上而不是属性值身上
+              因为每个属性同时只能出现一个input  row.saleAttrValueName代表input到时候收集的属性值名称数据，先暂存在属性身上,后面
+              用到了要从这个属性身上去拿的-->
+              <el-input
+                class="input-new-tag"
+                v-if="row.isEidt"
+                v-model="row.saleAttrValueName"
+                ref="saveTagInput"
+                size="small"
+              ></el-input>
+              <!--  @keyup.enter.native="handleInputConfirm"
+                @blur="handleInputConfirm" -->
+              <el-button v-else class="button-new-tag" size="small">+添加属性值</el-button>
+              <!--  @click="showInput" -->
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="150">
+            <template slot-scope="{row,$index}">
+              <HintButton size="mini" type="danger" icon="el-icon-delete" title="删除销售属性"></HintButton>
+            </template>
+          </el-table-column>
         </el-table>
       </el-form-item>
 
@@ -54,22 +88,21 @@
 <script>
 export default {
   name: "SpuForm",
-  props:['visible'],
+  props: ["visible"],
   data() {
     return {
       dialogImageUrl: "",
       dialogVisible: false,
-      spu:'',
-      spuInfo:{
-        spuName:'',
-        tmId:'',
-        description:'',
-
+      spu: "",
+      spuInfo: {
+        spuName: "",
+        tmId: "",
+        description: "",
+        spuSaleAttrList: [],
       },
-      spuImageList:[],
-      trademarkList:[],
-      baseSaleAttrList:[]
-
+      spuImageList: [],
+      trademarkList: [],
+      baseSaleAttrList: [],
     };
   },
   methods: {
@@ -81,51 +114,66 @@ export default {
       this.dialogVisible = true;
     },
     //点击父组件当中的修改按钮，父组件需要调用这个函数让子组件发请求获取初始化展示的数据
-    initUpdateSpuData(spu){
-      this.spu = spu  //保存从父组件修改传过来的某个spu 为了后面我们可以使用它内部的数据
+    initUpdateSpuData(spu) {
+      this.spu = spu; //保存从父组件修改传过来的某个spu 为了后面我们可以使用它内部的数据
 
       //http://localhost:9529/dev-api/admin/product/getSpuById/1122  获取对应id的spu详情
-      this.getSpuInfo(spu.id)
+      this.getSpuInfo(spu.id);
       //http://localhost:9529/dev-api/admin/product/spuImageList/1122 获取对应id的spu的图片列表
-      this.getSpuImageList(spu.id)
+      this.getSpuImageList(spu.id);
       //http://localhost:9529/dev-api/admin/product/baseTrademark/getTrademarkList 获取所有的品牌列表
-      this.getTrademarkList()  
+      this.getTrademarkList();
       //http://localhost:9529/dev-api/admin/product/baseSaleAttrList  获取所有的基础销售属性列表
-      this.getBaseSaleAttrList()
+      this.getBaseSaleAttrList();
     },
     //点击添加spu按钮，通知子组件发请求获取初始化展示数据
-    initAddSpuData(){
+    initAddSpuData() {
       //http://localhost:9529/dev-api/admin/product/baseTrademark/getTrademarkList 获取所有的品牌列表
-      this.getTrademarkList()  
+      this.getTrademarkList();
       //http://localhost:9529/dev-api/admin/product/baseSaleAttrList  获取所有的基础销售属性列表
-      this.getBaseSaleAttrList()
+      this.getBaseSaleAttrList();
     },
 
-    async getSpuInfo(spuId){
-      const result = await this.$API.spu.get(spuId)
-      this.spuInfo = result.data
+    async getSpuInfo(spuId) {
+      const result = await this.$API.spu.get(spuId);
+      this.spuInfo = result.data;
     },
-    async getSpuImageList(spuId){
-      const result = await this.$API.sku.getSpuImageList (spuId)
+    async getSpuImageList(spuId) {
+      const result = await this.$API.sku.getSpuImageList(spuId);
       //图片获取到的结构和最终upload要展示的结构不同，我们需要变为upload需要的结构
-      let spuImageList = result.data
-      spuImageList.forEach(item => {
-        item.name = item.imgName
-        item.url = item.imgUrl
-      })
-      this.spuImageList = spuImageList
+      let spuImageList = result.data;
+      spuImageList.forEach((item) => {
+        item.name = item.imgName;
+        item.url = item.imgUrl;
+      });
+      this.spuImageList = spuImageList;
     },
-    async getTrademarkList(){
-      const result = await this.$API.trademark.getList()
-      this.trademarkList = result.data
+    async getTrademarkList() {
+      const result = await this.$API.trademark.getList();
+      this.trademarkList = result.data;
     },
-    async getBaseSaleAttrList(){
-      const result = await this.$API.spu.getSaleList ()
-      this.baseSaleAttrList = result.data
-    } 
+    async getBaseSaleAttrList() {
+      const result = await this.$API.spu.getSaleList();
+      this.baseSaleAttrList = result.data;
+    },
   },
 };
 </script>
 
-<style scoped>
+<style>
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
 </style>
